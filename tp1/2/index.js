1282e71f696a36887ba667c0d8017a49558b18ea
totolartichaud <43410154+totolartichaud@users.noreply.github.com>
2019-10-23 11:16:45 +0200

    Update index.js

--- tp1/2/index.js
+++ tp1/2/index.js
@@ -7,6 +7,36 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+      
+    let nbboxes = input[0];
+    let actheight = 0;
+    let actboxes = 0;
+    let updboxes = 0;
+    let base = 1;
+    let totroad = 0;
+    let index = 0; 
+
+
+    while (updboxes != nbboxes) {
+        actheight += input[index];
+
+        base = 1;
+        index ++;
+        updboxes ++;
+
+        if (actheight > 100) {
+            base --;
+            updboxes --;
+            actheight = 0;
+            updboxes += actboxes;
+            actboxes = 0;
+            totroad ++;
+        }
+    }
+
+    totroad + 1;
+
+    return totroad;
 
     // AND HERE
   },
