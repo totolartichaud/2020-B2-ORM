1654bd337ee90f7b297d168b2f5532ad90245dac
totolartichaud <43410154+totolartichaud@users.noreply.github.com>
2019-10-23 11:15:55 +0200

    Update index.js

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,7 +7,27 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+      
+    let fplace = input[0];
+    let win = 0;
 
+        for (let index = 1; index < input.length; index ++) {
+            const element = input[index];
+            let move = element.split(" ");
+
+            fplace += parseInt(move[0], 10);
+            fplace -= parseInt(move[1], 10);
+        }
+
+        if (fplace >= 0 && fplace <= 100) {
+            win = 1000;
+        } else if (fplace > 100 && fplace <= 10000) {
+            win = 100;
+        } else {
+            win = "KO";
+        }
+
+        return win;
     // AND HERE
   },
   verify: function (dataset, output) {
